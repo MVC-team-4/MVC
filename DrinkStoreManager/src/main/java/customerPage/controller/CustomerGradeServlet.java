f6123e7b167c38ec28859a49b53accbad047b552
Reading-Snail <readingsnail17@gmail.com>
2022-11-11 12:15:27 +0900

    고객등급관리 완성

--- DrinkStoreManager/src/main/java/customerPage/controller/CustomerGradeServlet.java
+++ DrinkStoreManager/src/main/java/customerPage/controller/CustomerGradeServlet.java
@@ -12,7 +12,11 @@ import javax.servlet.http.HttpServletResponse;
 import customerPage.model.Customer;
 import customerPage.model.CustomerDao;
 import customerPage.model.CustomerGrade;
+import customerPage.model.CustomerGradeDao;
 import customerPage.model.CustomerService;
+import goodsPage.model.Goods;
+import goodsPage.model.GoodsDao;
+import goodsPage.model.GoodsService;
 
 @WebServlet("/CustomerGrade")
 public class CustomerGradeServlet extends HttpServlet{
@@ -21,24 +25,32 @@ public class CustomerGradeServlet extends HttpServlet{
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 		//고객 등급관리 화면으로 이동
+		CustomerGradeDao service = new CustomerGradeDao();
+		CustomerGrade grade = service.selectGrade();
+		request.setAttribute("grade", grade);
+		////    
+		
+		
 		request.getRequestDispatcher("WEB-INF/views/customer_grade.jsp").forward(request, response);
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//form 데이터 받아오기
+		CustomerGradeDao service = new CustomerGradeDao();
+		CustomerGrade grade = service.selectGrade();
+		
+		//수정 서비스
+		System.out.println("탔나?");
 		request.setCharacterEncoding("UTF-8");
 		String vvip = request.getParameter("vvip");
 		String vip = request.getParameter("vip");
 		String gold = request.getParameter("gold");
-		CustomerGrade grade = new CustomerGrade(vvip, vip, gold);
+		grade = new CustomerGrade(vvip, vip, gold);
 		System.out.println(grade);
 		
-		//수정 서비스
-		CustomerService service = new CustomerService(new CustomerDao());
-		service.updateGrade(grade);
-		
-		//조회 서블릿으로
-		response.sendRedirect("/DrinkStoreManager/Customerlist");
+		request.setAttribute("grade", grade);
+		//상품 조회 화면으로 이동
+		request.getRequestDispatcher("WEB-INF/views/customer_grade.jsp").forward(request, response);
 	}
 }
